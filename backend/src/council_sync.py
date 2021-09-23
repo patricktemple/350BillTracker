7476727bc97d936a1dc859114fd1c1416a75666b
Patrick Temple <patricktemple@gmail.com>
2021-09-23 17:16:12 -0400

    Update or add bill, not just add

--- backend/src/council_sync.py
+++ backend/src/council_sync.py
@@ -1,16 +1,31 @@
 from .council_api import find_bill
 from .models import Bill, db
+from sqlalchemy import update
+import logging
 
 
-def add_bill(intro_name):
+def add_or_update_bill(intro_name):
+    logging.info(f"Add or update bill for {intro_name}")
     matter = find_bill(intro_name)
 
-    bill = Bill(
-        file=matter["MatterFile"],
-        name=matter["MatterName"],
-        title=matter["MatterTitle"],
-        body=matter["MatterBodyName"],
-        intro_date=matter["MatterIntroDate"],
-        status=matter["MatterStatusName"],
-    )
-    db.session.add(bill)
+    data = {
+      "id": matter['MatterId'],
+      "file": matter["MatterFile"],
+      "name": matter["MatterName"],
+      "title": matter["MatterTitle"],
+      "body": matter["MatterBodyName"],
+      "intro_date": matter["MatterIntroDate"],
+      "status": matter["MatterStatusName"],
+    }
+
+    existing_bill = Bill.query.get(data['id'])
+    if existing_bill:
+        logging.info(f"Bill {intro_name} already in DB, updating")
+        for key in data.keys():
+            setattr(existing_bill, key, data[key])
+    else:
+        logging.info(f"Bill {intro_name} not found in DB, adding")
+        bill = Bill(**data)
+        db.session.add(bill)
+
+    db.session.commit()
