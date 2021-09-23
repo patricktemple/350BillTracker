ea8b78f132adb4c4737a18381181d7c1bbaf0395
Patrick Temple <patricktemple@gmail.com>
2021-09-23 16:16:25 -0400

    Add bills endpoint

--- backend/src/views.py
+++ backend/src/views.py
@@ -1,7 +1,9 @@
 from random import randrange
 
 from .app import app
+from .models import Bill
 from .council_api import get_recent_bills
+import json
 
 
 @app.route("/healthz", methods=["GET"])
@@ -14,3 +16,15 @@ def home():
     matters = get_recent_bills()
     random_matter = matters[randrange(0, len(matters))]
     return random_matter["MatterName"]
+
+
+@app.route("/bills", methods=["GET"])
+def bills():
+    bills = Bill.query.all()
+    return json.dumps([{
+      "name": b.name,
+      "title": b.title,
+      "file": b.file,
+      "status": b.status,
+      "body": b.body,
+    } for b in bills] )
