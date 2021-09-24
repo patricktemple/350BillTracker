c3ecb697d825384496418646c7b32d132ae74172
Patrick Temple <patricktemple@gmail.com>
2021-09-24 10:01:28 -0400

    Add prettier

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,18 +1,18 @@
 // Types returned by backend API. snake_case because that's what python does naturally?
 export interface Bill {
-    id: number;
-    file: string;
-    name: string;
-    title: string;
-    status: string;
-    body: string;
-  }
+  id: number;
+  file: string;
+  name: string;
+  title: string;
+  status: string;
+  body: string;
+}
 
 export interface CouncilMember {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 
-    // Or date?
-    term_start: string;
-    term_end: string;
+  // Or date?
+  term_start: string;
+  term_end: string;
 }
