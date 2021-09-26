d16c4884afcd01bd13b6062e380eae32be4b3750
Patrick Temple <patricktemple@gmail.com>
2021-09-26 16:35:54 -0400

    ahhh

--- backend/.platform/hooks/predeploy/migrate.sh
+++ backend/.platform/hooks/predeploy/migrate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -euo pipefail
+# set -euo pipefail
 
-# EB has not yet built the image via docker compose, so if we try to reference it here, it will use the previous one
-docker build ../../../var/app/staging -t legislation_app
-echo "About to run migration"
+# # EB has not yet built the image via docker compose, so if we try to reference it here, it will use the previous one
+# docker build ../../../var/app/staging -t legislation_app
+# echo "About to run migration"
 
-# this .env is the auto-inserted one by beanstalk, not any local one (which isn't in git repo anyway)
-docker run --env-file=../../../var/app/staging/.env --entrypoint alembic legislation_app upgrade head 2>&1
-echo "Migration done"
+# # this .env is the auto-inserted one by beanstalk, not any local one (which isn't in git repo anyway)
+# docker run --env-file=../../../var/app/staging/.env --entrypoint alembic legislation_app upgrade head 2>&1
+# echo "Migration done"
