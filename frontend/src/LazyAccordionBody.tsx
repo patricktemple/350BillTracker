c491230043a6bda1a278e716cdc9f395f177980b
Patrick Temple <patricktemple@gmail.com>
2021-09-28 17:40:57 -0400

    Format

--- frontend/src/LazyAccordionBody.tsx
+++ frontend/src/LazyAccordionBody.tsx
@@ -5,7 +5,10 @@ import AccordionContext from 'react-bootstrap/AccordionContext';
 // An Accordion.Body that does not mount its contents to the DOM until the first
 // time it is expanded. Useful for delaying unnecessary API calls if they are
 // required to render it.
-export default function LazyAccordionBody(props: {eventKey?: string, children: ReactElement }) {
+export default function LazyAccordionBody(props: {
+  eventKey?: string;
+  children: ReactElement;
+}) {
   const { activeEventKey } = useContext(AccordionContext);
   const [displayed, setDisplayed] = useState<boolean>(false);
 
@@ -14,7 +17,5 @@ export default function LazyAccordionBody(props: {eventKey?: string, children: R
       setDisplayed(true);
     }
   }, [displayed, setDisplayed, activeEventKey, props.eventKey]);
-  return (<Accordion.Body>
-    {displayed ? props.children : null}
-    </Accordion.Body>);
+  return <Accordion.Body>{displayed ? props.children : null}</Accordion.Body>;
 }
