196f91b0c1ff8ad027050396142beaf2997af7a2
Patrick Temple <patricktemple@gmail.com>
2021-10-04 15:35:00 -0400

    Starting to redo basic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,42 +8,56 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import { MdPeople, MdDescription } from 'react-icons/md';
+import { MdPeople, MdDescription, MdSettings } from 'react-icons/md';
 import styles from './style/App.module.scss';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className={styles.container}>
-        <div className={styles.heading}>350 Brooklyn Bill Tracker</div>
-        <div className={styles.leftNav}>
-          <Nav defaultActiveKey="/" className="flex-column">
-            <Nav.Link href="/" className={styles.navLink}>
-              <MdDescription className={styles.icon} />
-              Bills
-            </Nav.Link>
-            <Nav.Link href="/council-members" className={styles.navLink}>
-              <MdPeople className={styles.icon} />
-              Council members
-            </Nav.Link>
-          </Nav>
-        </div>
-        <div className={styles.mainContent}>
-          <main>
-            <Route path="/" exact>
-              <Redirect to="/saved-bills" />
-            </Route>
-            <Route
-              path="/saved-bills/:billId?"
-              exact
-              component={SavedBillsPage}
-            />
-            <Route
-              path="/council-members/:legislatorId?"
-              component={LegislatorsPage}
-            />
-          </main>
-        </div>
+        <div className={styles.leftNavBackground} />
+        <div className={styles.appTitle}>350 Brooklyn Bill Tracker</div>
+        <Link to="/" className={styles.billsLogo}>
+          <MdDescription />
+        </Link>
+        <Link to="/" className={styles.billsLink}>
+          Bills
+        </Link>
+        <Link to="/council-members" className={styles.legislatorsLogo}>
+          <MdPeople />
+        </Link>
+        <Link to="/council-members" className={styles.legislatorsLink}>
+          Council members
+        </Link>
+        <Link to="/setting" className={styles.settingsLogo}>
+          <MdSettings />
+        </Link>
+        <Link to="/settings" className={styles.settingsLink}>
+          Settings
+        </Link>
+        <Link to="/logout" className={styles.logoutLogo}>
+          <MdSettings />
+        </Link>
+        <Link to="/settings" className={styles.logoutLink}>
+          Logout
+        </Link>
+        {/* <div className={styles.content}> */}
+        <main className={styles.content}>
+          <Route path="/" exact>
+            <Redirect to="/saved-bills" />
+          </Route>
+          <Route
+            path="/saved-bills/:billId?"
+            exact
+            component={SavedBillsPage}
+          />
+          <Route
+            path="/council-members/:legislatorId?"
+            component={LegislatorsPage}
+          />
+        </main>
+        {/* </div> */}
       </div>
     </Router>
   );
