b76cdd8d5e3cb9c66067fa99f8151b5356baeed2
patricktemple <patricktemple@gmail.com>
2021-10-05 20:21:03 -0400

    Clean up login emails (#17)
    * Switch to jinja templates
    
    * Use app title from env var

--- backend/src/ses.py
+++ backend/src/ses.py
@@ -5,33 +5,20 @@ import logging
 from boto3 import client
 from botocore.exceptions import ClientError
 from werkzeug import exceptions
+from flask import render_template
+from .settings import APP_TITLE
 
 # This guide was important in getting the email address set up:
 # https://medium.com/responsetap-engineering/easily-create-email-addresses-for-your-route53-custom-domain-589d099dd0f2
 client = client("ses")
 
-SENDER = "350 Bill Tracker <no-reply@350billtracker.com>"
+SENDER = f"{APP_TITLE} <no-reply@350billtracker.com>"
 CHARSET = "UTF-8"
 
 
 def send_login_link_email(email_address, login_link):
-    # The email body for recipients with non-HTML email clients.
-    BODY_TEXT = (
-        "350 Bill Tracker.\r\n"
-        f"Go to the following link to log in:\r\n{login_link}"
-    )
-
-    # TODO: Use better template
-    # The HTML body of the email.
-    BODY_HTML = (
-        """<html>
-             <head></head>
-             <body>
-             <h1>Log in to 350 Bill tracker</h1>"""
-        f'<p>Click <a href="{login_link}">here</a> to log in.</p>'
-        """</body>
-      </html>"""
-    )
+    BODY_TEXT = render_template("email.txt", login_link=login_link)
+    BODY_HTML = render_template("email.html", login_link=login_link)
 
     try:
         response = client.send_email(
