fef84c34bf869fe7f3489a5a06e55ac25c6da694
Patrick Temple <patricktemple@gmail.com>
2021-10-14 16:20:27 -0400

    fix weird assert deletions

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -27,23 +27,30 @@ def test_create_user_success(client, user_id):
         "/api/users",
         data={"name": "Second user", "email": "second@example.com"},
     )
+    assert response.status_code == 200
 
     user = User.query.filter(User.id != user_id).one()
+    assert user.name == "Second user"
+    assert user.email == "second@example.com"
 
 
 def test_invite_user_email_exists(client, user_email):
     response = client.post(
         "/api/users", data={"name": "Second user", "email": user_email}
     )
+    assert response.status_code == 422
 
     # Should be case insensitive
     response = client.post(
         "/api/users", data={"name": "Second user", "email": user_email.upper()}
     )
+    assert response.status_code == 422
 
 
 def test_delete_user_success(client, user_id):
     response = client.delete(f"/api/users/{user_id}")
+    assert response.status_code == 200
+    assert not User.query.get(user_id)
 
 
 def test_delete_user_cant_be_deleted(client):
@@ -54,6 +61,7 @@ def test_delete_user_cant_be_deleted(client):
     db.session.commit()
 
     response = client.delete(f"/api/users/{new_user.id}")
+    assert response.status_code == 422
 
 
 def test_get_viewer(client, user_id, user_email):
