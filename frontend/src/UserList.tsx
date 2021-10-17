87b3dee3e77b5844f809c597459dcc53d856d8bd
Patrick Temple <patricktemple@gmail.com>
2021-10-17 08:41:58 -0500

    Show confirmation dialog befor deleting users.

--- frontend/src/UserList.tsx
+++ frontend/src/UserList.tsx
@@ -1,5 +1,7 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 import { User, Uuid } from './types';
+import Button from 'react-bootstrap/Button';
 
 import styles from './style/UserList.module.scss';
 import { ReactComponent as TrashIcon } from './assets/trash.svg';
@@ -10,6 +12,14 @@ interface Props {
 }
 
 export default function UserList(props: Props): ReactElement {
+  const [userIdConfirmingDelete, setUserIdConfirmingDelete] =
+    useState<Uuid | null>(null);
+
+  function handleConfirmDelete(id: Uuid) {
+    setUserIdConfirmingDelete(null);
+    props.handleDelete(id);
+  }
+
   return (
     <div className={styles.container}>
       <div>&nbsp;</div>
@@ -18,7 +28,7 @@ export default function UserList(props: Props): ReactElement {
       {props.users.map((user) => (
         <>
           {user.canBeDeleted ? (
-            <TrashIcon onClick={() => props.handleDelete(user.id)} />
+            <TrashIcon onClick={() => setUserIdConfirmingDelete(user.id)} />
           ) : (
             <div />
           )}
@@ -26,6 +36,32 @@ export default function UserList(props: Props): ReactElement {
           <div>{user.email}</div>
         </>
       ))}
+      <Modal
+        show={!!userIdConfirmingDelete}
+        onHide={() => setUserIdConfirmingDelete(null)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Remove user?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to remove this person&apos;s access to 350
+          Brooklyn Bill Tracker?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => setUserIdConfirmingDelete(null)}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => handleConfirmDelete(userIdConfirmingDelete!)}
+          >
+            Remove
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
