88ec3f9d26bb597cf38e7ac9d45247f966b9b735
Patrick Temple <patricktemple@gmail.com>
2022-01-03 16:47:25 -0500

    fix exiting bill tests

--- backend/tests/test_bills.py
+++ backend/tests/test_bills.py
@@ -32,6 +32,7 @@ def test_get_bills_unauthorized(unauthenticated_client):
 def test_get_saved_bills(client):
     bill = Bill(
         name="name",
+        description="description",
         nickname="ban gas",
         notes="Good job everyone",
         type=Bill.BillType.CITY,
@@ -42,7 +43,6 @@ def test_get_saved_bills(client):
         council_body="Committee on environment",
         status="Enacted",
         intro_date=now(),
-        title="title",
         active_version="A",
     )
     db.session.add(bill)
@@ -61,10 +61,13 @@ def test_get_saved_bills(client):
                     "cityBillId": 1,
                     "councilBody": "Committee on environment",
                     "file": "file",
-                    "title": "title",
                     "status": "Enacted",
+                    "sponsorCount": 0,
                 },
+                "codeName": "file",
+                "status": "Enacted",
                 "name": "name",
+                "description": "description",
                 "nickname": "ban gas",
                 "notes": "Good job everyone",
                 "twitterSearchTerms": DEFAULT_TWITTER_SEARCH_TERMS,
@@ -75,11 +78,10 @@ def test_get_saved_bills(client):
 
 
 def test_delete_bill(client):
-    bill = Bill(name="name", type=Bill.BillType.CITY)
+    bill = Bill(name="name", description="description", type=Bill.BillType.CITY)
     bill.city_bill = CityBill(
         city_bill_id=1,
         file="file",
-        title="title",
         status="Enacted",
         intro_date=now(),
         active_version="A",
@@ -100,11 +102,10 @@ def test_delete_bill(client):
 
 def test_update_bill(client):
     bill_id = uuid4()
-    bill = Bill(id=bill_id, name="name", type=Bill.BillType.CITY)
+    bill = Bill(id=bill_id, name="name", description="description", type=Bill.BillType.CITY)
     bill.city_bill = CityBill(
         city_bill_id=1,
         file="file",
-        title="title",
         status="Enacted",
         intro_date=now(),
         active_version="A",
@@ -130,14 +131,17 @@ def test_update_bill(client):
             {
                 "id": str(bill_id),
                 "type": "CITY",
+                "description": "description",
                 "stateBill": None,
                 "cityBill": {
                     "cityBillId": 1,
                     "councilBody": None,
                     "file": "file",
                     "status": "Enacted",
-                    "title": "title",
+                    "sponsorCount": 0,
                 },
+                "codeName": "file",
+                "status": "Enacted",
                 "name": "name",
                 "nickname": "skip the stuff",
                 "notes": "good bill",
@@ -197,11 +201,10 @@ def test_save_bill(client):
 
 @responses.activate
 def test_save_bill__already_exists(client):
-    bill = Bill(name="name", type=Bill.BillType.CITY)
+    bill = Bill(name="name", description="description", type=Bill.BillType.CITY)
     bill.city_bill = CityBill(
         city_bill_id=1,
         file="file",
-        title="title",
         intro_date=now(),
         status="Committee",
         active_version="A",
@@ -225,7 +228,7 @@ def test_lookup_bill_not_tracked(client):
     )
 
     response = client.get(
-        "/api/search-bills?file=1234",
+        "/api/city-bills/search?file=1234",
     )
     assert response.status_code == 200
     response_data = json.loads(response.data)[0]
@@ -238,11 +241,10 @@ def test_lookup_bill_not_tracked(client):
 
 @responses.activate
 def test_lookup_bill_already_tracked(client):
-    bill = Bill(name="name", type=Bill.BillType.CITY)
+    bill = Bill(name="name", description="description", type=Bill.BillType.CITY)
     bill.city_bill = CityBill(
         city_bill_id=1,
         file="file",
-        title="title",
         intro_date=now(),
         status="Enacted",
         active_version="A",
@@ -257,7 +259,7 @@ def test_lookup_bill_already_tracked(client):
     )
 
     response = client.get(
-        "/api/search-bills?file=1234",
+        "/api/city-bills/search?file=1234",
     )
     # assert more fields?
     assert response.status_code == 200
