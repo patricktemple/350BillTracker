e80ef1fff1dc2bc75c23df07e68ad30379e7a7b7
Patrick Temple <patricktemple@gmail.com>
2022-01-10 21:47:50 -0500

    fix scraper

--- backend/tools/scrape_state_data.py
+++ backend/tools/scrape_state_data.py
@@ -1,3 +1,11 @@
+"""
+Scrapes the senate and assembly websites for contact info and other data about
+representatives. Prints out a Python dict containing this data.
+
+This dict should be placed into the static_data/senate_data.py and static_data/assembly_data.py
+after visually inspecting it for any obvious mistakes.
+"""
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -7,54 +15,51 @@ from src.app import app
 from src.person.models import Person, Senator, AssemblyMember
 import json
 
-# TODO comment this script
+
+SENATE_URL_ROOT = "https://www.nysenate.gov"
+
+def _extract_senate_contact_info(address_section):
+    phone = address_section.find('span', string="Phone: ")
+    fax = address_section.find('span', string="Fax: ")
+    city = address_section.find('span', class_="locality")
+    return {
+        "phone": phone and phone.next_sibling,
+        "fax": fax and fax.next_sibling,
+        "city": re.compile('\w+[\s\w]*').search(city.string).group()
+    }
 
 def get_senate_data():
-    senator_list_html = requests.get('https://www.nysenate.gov/senators-committees').text
-
-    url_root = "https://www.nysenate.gov"
-
+    print("Fetching main Senate directory page")
+    senator_list_html = requests.get(f"{SENATE_URL_ROOT}/senators-committees").text
     list_soup = BeautifulSoup(senator_list_html)
 
-    senator_urls = set()
+    senator_urls_and_parties = set()
     for link in list_soup.find_all('a'):
         href = link.get('href')
         if href is not None and href.startswith('/senators/'):
             party_text = str(link.find('span', class_='nys-senator--party').string)
-            # print(party_text)
             if re.compile('[\( ]D[,\)]').search(party_text):
-                # print(party_text)
-                party = "D" # THIS IS WRONG, all people are D here?
+                party = "D"
             elif re.compile('[\( ]R[,\)]').search(party_text):
                 party = "R"
             else:
                 party = None
 
-            senator_urls.add((url_root + href + "/contact", party))
-
-    # print(senator_urls)
-
-    def extract_contact_info(address_section):
-        phone = address_section.find('span', string="Phone: ")
-        fax = address_section.find('span', string="Fax: ")
-        city = address_section.find('span', class_="locality")
-        return {
-            "phone": phone and phone.next_sibling,
-            "fax": fax and fax.next_sibling,
-            "city": re.compile('\w+[\s\w]*').search(city.string).group()
-        }
+            senator_urls_and_parties.add((SENATE_URL_ROOT + href + "/contact", party))
 
     output = {}
+    for contact_url, party in senator_urls_and_parties:
+        print(f"Fetching senator detail page: {contact_url}")
 
-    for contact_url, party in senator_urls:
-        print(contact_url, party)
         contact_page = requests.get(contact_url).text
         contact_soup = BeautifulSoup(contact_page)
-        district = contact_soup.find_all('a', href=lambda href: href and href.startswith('/district/'))[0]['href'][len('/district/'):]
-        # print(district)
+
+        # This page has multiple divs each containing full address info. One is for Albany and the
+        # rest are for the district.
+        district_number = contact_soup.find_all('a', href=lambda href: href and href.startswith('/district/'))[0]['href'][len('/district/'):]
         address_tags = contact_soup.find_all('div', class_='location vcard')
-        district_tags = [t for t in address_tags if 'district office' in str(t).lower()]
-        albany_tags = [t for t in address_tags if 'albany office' in str(t).lower()]
+        district_divs = [t for t in address_tags if 'district office' in str(t).lower()]
+        albany_divs = [t for t in address_tags if 'albany office' in str(t).lower()]
 
         email = contact_soup.find('div', class_='c-block--senator-email').find('div', class_='field-content').find('a').string
 
@@ -64,14 +69,14 @@ def get_senate_data():
             "albany_contact": [],
             "name": contact_soup.find('span', class_="c-senator-hero--name").find('a').string,
             "website": contact_url,
-            "district": district,
+            "district": district_number,
             "party": party
         }
-        for d in district_tags:
-            results['district_contact'].append(extract_contact_info(d))
+        for d in district_divs:
+            results['district_contact'].append(_extract_senate_contact_info(d))
         
-        for a in albany_tags:
-            results['albany_contact'].append(extract_contact_info(a))
+        for a in albany_divs:
+            results['albany_contact'].append(_extract_senate_contact_info(a))
         
         output[results['district']] = results
 
@@ -79,27 +84,28 @@ def get_senate_data():
 
 
 def get_assembly_data():
+    f"Fetching Assembly data"
     assembly_list_html = requests.get('https://www.nyassembly.gov/mem').text
-    # print(assembly_list_html)
 
     soup = BeautifulSoup(assembly_list_html)
 
-    items = soup.find_all('section', class_='mem-item')
-    print(len(items))
+    assembly_member_sections = soup.find_all('section', class_='mem-item')
 
     output = {}
-    for item in items:
-        # print(item)
+    for assembly_member_section in assembly_member_sections:
         result = {}
-        info = item.find('div', class_='mem-info')
+
+        info = assembly_member_section.find('div', class_='mem-info')
         result['name'] = next(info.find('h3', class_='mem-name').find('a').strings).strip()
+    
         email_container = info.find('div', class_='mem-email')
         result['email'] = email_container and email_container.find('a').string.strip()
-        address = item.find('div', class_='mem-address')
-        addresses = item.find_all('div', class_='full-addr notranslate')
+    
+        # address = assembly_member_section.find('div', class_='mem-address')
+        addresses = assembly_member_section.find_all('div', class_='full-addr notranslate')
 
-        district_result = re.compile('District (\d+)').search(str(item))
-        result['district'] = district_result and district_result.group(1)
+        district_match = re.compile('District (\d+)').search(str(assembly_member_section))
+        result['district'] = district_match and district_match.group(1)
 
         result['district_contact'] = []
         result['albany_contact'] = []
@@ -115,7 +121,6 @@ def get_assembly_data():
                         addr['phone'] = re_result.group()
                 elif match := re.compile('^\s*(\w+[\w\s]*), NY').search(line):
                     city = match.group(1)
-                    print(city)
                     addr['city'] = city
             if 'LOB' in lines[0]:
                 result['albany_contact'].append(addr)
@@ -125,13 +130,9 @@ def get_assembly_data():
         output[result['district']] = result
     return output
 
-# print(get_senate_data())
-
 data = {'senate': get_senate_data(), 'assembly': get_assembly_data()}
-# print(get_assembly_data())
 
-# TWO STEPS
-
+# Now join them against the DB to line them up against the state member ID used by the API.
 senators = Senator.query.all()
 senator_data = {}
 for senator in senators:
@@ -140,11 +141,15 @@ for senator in senators:
         **matching_item,
         "name": senator.person.name,
         "district": senator.district,
+
+        # This should be visually inspected every time the scraper is re-run,
+        # before the data is accepted.
         "scrape_name__SANITY_CHECK": matching_item['name'],
     }
 
-print("Senate")
+print("Senate\n")
 print(senator_data)
+print()
 
 
 assembly_members = AssemblyMember.query.all()
@@ -162,5 +167,5 @@ for member in assembly_members:
         "scrape_name__SANITY_CHECK": matching_item['name'],
     }
 
-print("Assembly")
+print("Assembly\n")
 print(assembly_data)
