c36c7e7fe500a6a219af8f4e819e655a07c070eb
Patrick Temple <patricktemple@gmail.com>
2022-03-01 13:46:14 -0500

    simplify and pull out bottom nav link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,6 @@ import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import BillListPage from './pages/BillListPage';
 import PersonsPage from './pages/PersonsPage';
 import styles from './style/App.module.scss';
-import { Link } from 'react-router-dom';
-import { ReactComponent as SettingsIcon } from './assets/settings.svg';
-import { ReactComponent as LogoutIcon } from './assets/logout.svg';
-import { ReactComponent as BillsIcon } from './assets/paper.svg';
-import { ReactComponent as LegislatorIcon } from './assets/person.svg';
-import AppLogo from './assets/app-logo.png';
 import React, { useContext } from 'react';
 import RequestLoginLinkPage from './pages/RequestLoginLinkPage';
 import { AuthContextProvider, AuthContext } from './AuthContext';
@@ -16,6 +10,8 @@ import { useLocation } from 'react-router-dom';
 import LoginFromTokenPage from './pages/LoginFromTokenPage';
 import SettingsPage from './pages/SettingsPage';
 import BillDetailsPage from './pages/BillDetailsPage';
+import LeftNav from './components/LeftNav';
+import { setSyntheticLeadingComments } from 'typescript';
 
 function AppContent() {
   const authContext = useContext(AuthContext);
@@ -38,46 +34,21 @@ function AppContent() {
 
   return (
     <Router>
-      <div className={styles.container}>
-        <div className={styles.leftNavBackground} />
-        <div className={styles.appTitle}>
-          <img src={AppLogo} alt="Logo" className={styles.appLogo} />
-          <h2>Bill Tracker</h2>
+      <div className={styles.pageContainer}>
+        <div className={styles.leftNav}>
+          <LeftNav onLogout={handleLogout} />
         </div>
-        <Link to="/" className={styles.billsLogo}>
-          <BillsIcon />
-        </Link>
-        <Link to="/" className={styles.billsLink}>
-          Bills
-        </Link>
-        <Link to="/people" className={styles.legislatorsLogo}>
-          <LegislatorIcon />
-        </Link>
-        <Link to="/people" className={styles.legislatorsLink}>
-          People
-        </Link>
-        <Link to="/setting" className={styles.settingsLogo}>
-          <SettingsIcon />
-        </Link>
-        <Link to="/settings" className={styles.settingsLink}>
-          Settings
-        </Link>
-        <a href="#" onClick={handleLogout} className={styles.logoutLogo}>
-          <LogoutIcon />
-        </a>
-        <a href="#" onClick={handleLogout} className={styles.logoutLink}>
-          Logout
-        </a>
-        <main className={styles.content}>
-          <Route exact path="/">
-            <Redirect to="/bills" />
-          </Route>
-          <Route exact path="/bills" component={BillListPage} />
-          <Route path="/bills/:billId" component={BillDetailsPage} />
-          <Route path="/people/:personId?" component={PersonsPage} />
-          <Route path="/settings" component={SettingsPage} />
-        </main>
-      </div>
+          
+          <main className={styles.content}>
+            <Route exact path="/">
+              <Redirect to="/bills" />
+            </Route>
+            <Route exact path="/bills" component={BillListPage} />
+            <Route path="/bills/:billId" component={BillDetailsPage} />
+            <Route path="/people/:personId?" component={PersonsPage} />
+            <Route path="/settings" component={SettingsPage} />
+          </main>
+        </div>
     </Router>
   );
 }
---
+++ frontend/src/components/LeftNav.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { ReactComponent as SettingsIcon } from '../assets/settings.svg';
+import { ReactComponent as LogoutIcon } from '../assets/logout.svg';
+import { ReactComponent as BillsIcon } from '../assets/paper.svg';
+import { ReactComponent as PersonIcon } from '../assets/person.svg';
+import AppLogo from '../assets/app-logo.png';
+import React from 'react';
+
+import styles from '../style/components/LeftNav.module.scss';
+
+export interface Props {
+    onLogout: (event: any) => void;
+}
+
+export default function LeftNav(props: Props) {
+    return (
+            <div className={styles.container}>
+                <div className={styles.appTitle}>
+                    <img src={AppLogo} alt="Logo" className={styles.appLogo} />
+                    <h1>Bill Tracker</h1>
+                </div>
+                <div className={styles.mainLinkSection}>
+                    <Link to="/" className={styles.mainLinkItem}>
+                        <BillsIcon />
+                        Bills
+                    </Link>
+                    <Link to="/people" className={styles.mainLinkItem}>
+                        <PersonIcon />
+                        People
+                    </Link>
+                </div>
+                <div className={styles.bottomLinkSection}>
+                    <Link to="/setting" className={styles.bottomLinkItem}>
+                        <SettingsIcon /> Settings
+                    </Link>
+                    <a href="#" onClick={props.onLogout} className={styles.bottomLinkItem}>
+                        <LogoutIcon /> Logout
+                    </a>
+                </div>
+            </div>
+    );
+}
