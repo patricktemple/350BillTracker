7ef78c0e4ee2a42046d99f5f9f77f558806cd3be
Patrick Temple <patricktemple@gmail.com>
2022-03-17 18:45:47 -0400

    added one state test

--- backend/tests/test_power_hour_sheets.py
+++ backend/tests/test_power_hour_sheets.py
@@ -1,19 +1,21 @@
 from unittest.mock import MagicMock, patch
 from uuid import uuid4
 
+import pytest
+
 from src.models import db
-from src.person.models import CouncilMember, OfficeContact, Person
+from src.person.models import CouncilMember, OfficeContact, Person, Senator, AssemblyMember
 from src.power_hours import (
     _extract_data_from_previous_spreadsheet,
     create_power_hour,
     CITY_COLUMN_TITLE_SET,
 )
-from src.sponsorship.models import CitySponsorship
+from src.sponsorship.models import CitySponsorship, SenateSponsorship, AssemblySponsorship
 
 
 @patch("src.google_sheets.Credentials")
 @patch("src.google_sheets.build")
-def test_generate_google_sheet__no_import(
+def test_create_city_power_hour__no_import(
     mock_build, mock_credentials, snapshot, city_bill
 ):
     mock_sheets_service = MagicMock()
@@ -83,7 +85,7 @@ def test_generate_google_sheet__no_import(
 
 @patch("src.google_sheets.Credentials")
 @patch("src.google_sheets.build")
-def test_generate_google_sheet__with_import(
+def test_create_city_power_hour__with_import(
     mock_build, mock_credentials, snapshot, city_bill
 ):
     mock_sheets_service = MagicMock()
@@ -163,6 +165,96 @@ def test_generate_google_sheet__with_import(
     # Needs a matcher.
     mock_sheets_service.spreadsheets().create.assert_called_with(body=snapshot)
     mock_sheets_service.spreadsheets().create().execute.assert_called()
+
+@pytest.mark.parametrize(
+    ("senate_bill_exists", "assembly_bill_exists"), [(False, True), (True, True), (True, False)]
+)
+@patch("src.google_sheets.Credentials")
+@patch("src.google_sheets.build")
+def test_create_state_power_hour__no_import(
+    mock_build, mock_credentials, snapshot, state_bill, senator, assembly_member, senate_bill_exists, assembly_bill_exists
+):
+    mock_sheets_service = MagicMock()
+    mock_drive_service = MagicMock()
+
+    # TODO: Figure out how to reuse this as a fixture
+    def side_effect(service, version, credentials=None):
+        if service == "sheets":
+            return mock_sheets_service
+        if service == "drive":
+            return mock_drive_service
+        raise ValueError("wrong service")
+
+    mock_build.side_effect = side_effect
+
+    if senate_bill_exists:
+        non_sponsor_senator = Person(
+            id=uuid4(), name="Non sponsor senator", type=Person.PersonType.SENATOR
+        )
+        non_sponsor_senator.senator = Senator(state_member_id=1)
+        non_sponsor_senator.office_contacts.append(
+            OfficeContact(
+                type=OfficeContact.OfficeContactType.CENTRAL_OFFICE,
+                phone="111-222-3333",
+                fax="123-567-1890",
+                city="Albany"
+            ),
+        )
+        db.session.add(non_sponsor_senator)
+
+        # The sponsor is fully defined from the "senator" fixture already
+        db.session.add(
+            SenateSponsorship(
+                person_id=senator.id,
+                bill_id=state_bill.id,
+                is_lead_sponsor=True
+            )
+        )
+    else:
+        state_bill.senate_bill = None
+
+    if assembly_bill_exists:
+        non_sponsor_assembly_member = Person(
+            id=uuid4(), name="Non sponsor assembly member", type=Person.PersonType.ASSEMBLY_MEMBER
+        )
+        non_sponsor_assembly_member.assembly_member = AssemblyMember(state_member_id=2)
+        non_sponsor_assembly_member.office_contacts.append(
+            OfficeContact(
+                type=OfficeContact.OfficeContactType.DISTRICT_OFFICE,
+                phone="111-222-444",
+                fax="123-567-800",
+                city="Manhattan"
+            ),
+        )
+        db.session.add(non_sponsor_assembly_member)
+        db.session.add(
+            AssemblySponsorship(
+                person_id=assembly_member.id,
+                bill_id=state_bill.id,
+                is_lead_sponsor=False
+            )
+        )
+    else:
+        state_bill.assembly_bill = None  
+    db.session.commit()
+
+    mock_sheets_service.spreadsheets().create().execute.return_value = {
+        "spreadsheetId": "test_spreadsheet"
+    }
+
+    _, messages = create_power_hour(
+        state_bill.id, title="My power hour", old_spreadsheet_to_import=None
+    )
+    assert messages == ["Spreadsheet was created"]
+
+    mock_sheets_service.spreadsheets().create.assert_called_with(body=snapshot)
+    mock_sheets_service.spreadsheets().create().execute.assert_called()
+
+    mock_drive_service.permissions().create.assert_called_with(
+        fileId="test_spreadsheet",
+        fields="id",
+        body={"type": "anyone", "role": "writer"},
+    )
 
 
 def test_extract_data_from_previous_spreadsheet():
