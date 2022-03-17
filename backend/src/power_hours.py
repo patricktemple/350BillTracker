e4037d7af526a07940dfbc47f476f20775c6d5b2
Patrick Temple <patricktemple@gmail.com>
2022-03-17 13:37:04 -0400

    first attempt at backend of basic state power hour, has major holes

--- backend/src/power_hours.py
+++ backend/src/power_hours.py
@@ -8,18 +8,13 @@ from googleapiclient.discovery import build
 from sqlalchemy.orm import selectinload
 
 from . import settings, twitter
-from .bill.models import CityBill
+from .bill.models import CityBill, StateBill, SenateBill, AssemblyBill, Bill
 from .models import UUID
-from .person.models import CouncilMember, OfficeContact, Person
-from .sponsorship.models import CitySponsorship
+from .person.models import CouncilMember, OfficeContact, Person, Senator, AssemblyMember
+from .sponsorship.models import CitySponsorship, SenateSponsorship, AssemblySponsorship
 from .google_sheets import  Cell, get_spreadsheet_cells, create_spreadsheet, create_row_data, create_bolded_row_data, get_spreadsheet_cells
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-]
-
-COLUMN_TITLES = [
+CITY_COLUMN_TITLES = [
     "",
     "Name",
     "Email",
@@ -32,7 +27,34 @@ COLUMN_TITLES = [
     "Staffers",
     "Twitter search\nNote: Due to a Twitter bug, the Twitter search sometimes displays 0 results even when there should be should be matching tweets. Refreshing the Twitter page often fixes this.",
 ]
-COLUMN_TITLE_SET = set(COLUMN_TITLES)
+CITY_COLUMN_TITLE_SET = set(CITY_COLUMN_TITLES)
+
+STATE_COLUMN_TITLES = [
+    "",
+    "Name",
+    "Email",
+    "Party",
+    "District",
+    "District Contact",
+    "Albany Contact",
+    "Twitter",
+    # "Committees",
+    "Staffers",
+    # "Twitter search\nNote: Due to a Twitter bug, the Twitter search sometimes displays 0 results even when there should be should be matching tweets. Refreshing the Twitter page often fixes this.",
+]
+CITY_COLUMN_TITLES = [
+    "",
+    "Name",
+    "Email",
+    "Party",
+    "Borough",
+    "District Phone",
+    "Legislative Phone",
+    "Twitter",
+    "Committees",
+    "Staffers",
+    "Twitter search\nNote: Due to a Twitter bug, the Twitter search sometimes displays 0 results even when there should be should be matching tweets. Refreshing the Twitter page often fixes this.",
+]
 
 BOROUGH_SORT_TABLE = {
     "Brooklyn": 0,
@@ -45,7 +67,7 @@ BOROUGH_SORT_TABLE = {
 
 # Width in pixels for each column. We can't dynamically set it to match the,
 # contents via the API, so instead we just hardcode them as best we can.
-COLUMN_WIDTHS = [150, 150, 200, 50, 100, 100, 150, 200, 250, 250, 250]
+CITY_COLUMN_WIDTHS = [150, 150, 200, 50, 100, 100, 150, 200, 250, 250, 250]
 
 
 @dataclass
@@ -70,9 +92,25 @@ def _get_staffer_display_string(staffer: Person):
     return f"{title_string}{staffer.name} ({contact_string})"
 
 
-def _create_legislator_row(
+def _get_imported_data_cells(person, import_data):
+    cells = []
+    legislator_data = import_data.column_data_by_legislator_id.get(
+        person.id
+    )
+    if legislator_data is not None:
+        for extra_column in import_data.extra_column_titles:
+            text = legislator_data.get(extra_column, "")
+            cells.append(Cell(text))
+    else:
+        logging.warning(
+            f"No data for {person.name} in import data"
+        )   
+    return cells
+
+
+def _create_council_member_row(
     council_member: CouncilMember,
-    city_bill: CityBill,
+    bill: Bill,
     import_data: Optional[PowerHourImportData],
     is_lead_sponsor: bool = False,
 ):
@@ -83,7 +121,7 @@ def _create_legislator_row(
     staffer_text = "\n\n".join(staffer_strings)
 
     twitter_search_url = twitter.get_bill_twitter_search_url(
-        city_bill.bill, council_member.person
+        bill, council_member.person
     )
 
     legislative_phone = ", ".join(
@@ -132,24 +170,76 @@ def _create_legislator_row(
         ),
     ]
     if import_data:
-        legislator_data = import_data.column_data_by_legislator_id.get(
-            council_member.person_id
-        )
-        if legislator_data is not None:
-            for extra_column in import_data.extra_column_titles:
-                text = legislator_data.get(extra_column, "")
-                cells.append(Cell(text))
-        else:
-            logging.warning(
-                f"No legislator data for {council_member.person.name} in import data"
-            )
+        cells.extend(_get_imported_data_cells(council_member.person, import_data))
+
     return create_row_data(cells)
 
 
-def _create_phone_bank_row_data(
-    city_bill: CityBill,
+# TODO: Make generic to be all state reps
+# Can I dedupe this more?
+def _create_state_representative_row(
+    representative: Person,
+    bill: Bill,
+    import_data: Optional[PowerHourImportData],
+    is_lead_sponsor: bool = False,
+):
+    staffer_strings = [
+        _get_staffer_display_string(s)
+        for s in representative.staffer_persons
+    ]
+    staffer_text = "\n\n".join(staffer_strings)
+
+    twitter_search_url = twitter.get_bill_twitter_search_url(
+        bill, representative
+    )
+
+    # TODO: Improve contact info
+    legislative_phone = ", ".join(
+        (
+            c.phone
+            for c in representative.office_contacts
+            if c.phone
+            and c.type == OfficeContact.OfficeContactType.CENTRAL_OFFICE
+        )
+    )
+    district_phone = ", ".join(
+        (
+            c.phone
+            for c in representative.office_contacts
+            if c.phone
+            and c.type == OfficeContact.OfficeContactType.DISTRICT_OFFICE
+        )
+    )
+
+    cells = [
+        Cell(""),
+        Cell(
+            _get_sponsor_name_text(representative.name, is_lead_sponsor)
+        ),
+        Cell(representative.email),
+        Cell(representative.party),
+        Cell(district_phone),
+        Cell(legislative_phone),
+        Cell(
+            representative.display_twitter or "",
+            link_url=representative.twitter_url,
+        ),
+        Cell(staffer_text),
+        Cell(
+            "Relevant tweets" if twitter_search_url else "",
+            link_url=twitter_search_url,
+        ),
+    ]
+    if import_data:
+        cells.append(_get_imported_data_cells(representative, import_data))
+
+    return create_row_data(cells)
+
+
+def _create_city_power_hour_row_data(
+    bill: Bill,
     sponsorships: List[CitySponsorship],
-    non_sponsors: List[CitySponsorship],
+    non_sponsors: List[CouncilMember],
     import_data: Optional[PowerHourImportData],
 ):
     """Generates the full body payload that the Sheets API requires for a
@@ -158,13 +248,13 @@ def _create_phone_bank_row_data(
     extra_titles = import_data.extra_column_titles if import_data else []
     rows = [
         create_bolded_row_data(
-            COLUMN_TITLES + extra_titles,
+            CITY_COLUMN_TITLES + extra_titles,
         ),
         create_bolded_row_data(["NON-SPONSORS"]),
     ]
     for non_sponsor in non_sponsors:
         rows.append(
-            _create_legislator_row(non_sponsor, city_bill, import_data)
+            _create_council_member_row(non_sponsor, bill, import_data)
         )
 
     rows.append(create_row_data([]))
@@ -172,11 +262,49 @@ def _create_phone_bank_row_data(
 
     for sponsorship in sponsorships:
         rows.append(
-            _create_legislator_row(
+            _create_council_member_row(
                 sponsorship.council_member,
-                city_bill,
+                bill,
                 import_data,
                 sponsorship.sponsor_sequence == 0,
+            )
+        )
+    
+    return rows
+
+
+# TODO: Dedupe this with above!
+def _create_state_power_hour_row_data(
+    bill: Bill,
+    sponsorships: List[SenateSponsorship],
+    non_sponsors: List[Senator],
+    import_data: Optional[PowerHourImportData],
+):
+    """Generates the full body payload that the Sheets API requires for a
+    phone bank spreadsheet."""
+
+    extra_titles = import_data.extra_column_titles if import_data else []
+    rows = [
+        create_bolded_row_data(
+            CITY_COLUMN_TITLES + extra_titles,
+        ),
+        create_bolded_row_data(["NON-SPONSORS"]),
+    ]
+    for non_sponsor in non_sponsors:
+        rows.append(
+            _create_state_representative_row(non_sponsor.person, bill, import_data)
+        )
+
+    rows.append(create_row_data([]))
+    rows.append(create_bolded_row_data(["SPONSORS"]))
+
+    for sponsorship in sponsorships:
+        rows.append(
+            _create_state_representative_row(
+                sponsorship.senator.person,
+                bill,
+                import_data,
+                sponsorship.is_lead_sponsor,
             )
         )
     
@@ -194,25 +322,8 @@ def _get_sponsor_name_text(legislator_name, is_lead):
     return f"{legislator_name}{' (lead)' if is_lead else ''}"
 
 
-def create_power_hour(
-    bill_id: UUID, title: str, old_spreadsheet_to_import: str
-) -> Tuple[Dict, List[str]]:
-    """Creates a spreadsheet that's a template to run a phone bank
-    for a specific bill, based on its current sponsors. The sheet will be
-    owned by a robot Google account and will be made publicly editable by
-    anyone with the link."""
-    logging.info(
-        f"Creating new power hour titled {title}, importing old spreadsheet {old_spreadsheet_to_import}"
-    )
-    city_bill = (
-        CityBill.query.filter_by(bill_id=bill_id)
-        .options(
-            selectinload(CityBill.sponsorships),
-        )
-        .one()
-    )
-
-    sponsorships = city_bill.sponsorships
+def _create_city_power_hour(bill, sheet_title, import_data):
+    sponsorships = bill.city_bill.sponsorships
     sponsorships = sorted(
         sponsorships, key=lambda s: get_sort_key(s.council_member)
     )
@@ -223,6 +334,46 @@ def create_power_hour(
     ).all()
     non_sponsors = sorted(non_sponsors, key=get_sort_key)
 
+    row_data = _create_city_power_hour_row_data(
+        bill.city_bill, sponsorships, non_sponsors, import_data # unclear this needs to pass city_bill
+    )
+
+    return create_spreadsheet(sheet_title, row_data, CITY_COLUMN_WIDTHS)
+
+
+def _create_state_power_hour(bill, sheet_title, import_data):
+    # Make sure this works if only one of senate or assembly bill are defined
+    senate_bill = bill.state_bill.senate_bill
+    assembly_bill = bill.state_bill.assembly_bill
+
+    if senate_bill:
+        senate_sponsorships = senate_bill.sponsorships
+        senate_sponsor_ids = [s.person_id for s in senate_sponsorships]
+        senate_non_sponsors = Senator.query.filter(Senator.person_id.not_in(senate_sponsor_ids)).all()
+        # TODO repeat for assembly, make generic
+    else:
+        senate_sponsorships = []
+        senate_non_sponsors = []
+    
+    row_data = _create_state_power_hour_row_data(
+        bill, senate_sponsorships, senate_non_sponsors, import_data
+    )
+
+    return create_spreadsheet(sheet_title, row_data, []) # TODO: DO  state column widths
+
+
+def create_power_hour(
+    bill_id: UUID, title: str, old_spreadsheet_to_import: str
+) -> Tuple[Dict, List[str]]:
+    """Creates a spreadsheet that's a template to run a phone bank
+    for a specific bill, based on its current sponsors. The sheet will be
+    owned by a robot Google account and will be made publicly editable by
+    anyone with the link."""
+    bill = Bill.query.filter_by(id=bill_id).one()
+
+    logging.info(
+        f"Creating new power hour titled {title}, importing old spreadsheet {old_spreadsheet_to_import}"
+    )
     if old_spreadsheet_to_import:
         import_data = _extract_data_from_previous_power_hour(
             old_spreadsheet_to_import
@@ -230,11 +381,10 @@ def create_power_hour(
     else:
         import_data = None
 
-    row_data = _create_phone_bank_row_data(
-        city_bill, sponsorships, non_sponsors, import_data
-    )
-
-    spreadsheet_result = create_spreadsheet(title, row_data, COLUMN_WIDTHS)
+    if bill.type == Bill.BillType.CITY:
+        spreadsheet_result = _create_city_power_hour(bill, title, import_data)
+    else:
+        spreadsheet_result = _create_state_power_hour(bill, title, import_data)
 
     output_messages = import_data.import_messages if import_data else []
     output_messages.append("Spreadsheet was created")
@@ -258,7 +408,7 @@ def _extract_data_from_previous_spreadsheet(
     extra_column_title_indices: Tuple[int, str] = []
     name_column_index = None
     for i, title in enumerate(title_row):
-        if title not in COLUMN_TITLE_SET:
+        if title not in CITY_COLUMN_TITLE_SET:
             extra_column_title_indices.append((i, title))
         elif title == "Name":
             name_column_index = i
@@ -297,6 +447,7 @@ def _extract_data_from_previous_spreadsheet(
         )
 
     # Now rekey by legislator ID
+    # TODO: I think I need to change this so that it's by person_id, not the council_id
     council_members = CouncilMember.query.all()
     column_data_by_council_member_id: Dict[UUID, Dict[str, str]] = {}
     for council_member in council_members:
